1aef056a6b992082ebf37db378a959b9898de8da
Mikarific <mikerific2@gmail.com>
2023-06-14 06:11:10 -0600

    Change participating communities to my own list.

--- index.js
+++ index.js
@@ -41,26 +41,115 @@ gql.injectBuffer((req, buffer) => {
 	}
 
 	// Add r/PlacePride to the "Explore communities" tab.
-	if (data.data && data.data.navBarEventCommunityPicker) {
-		data.data.navBarEventCommunityPicker.communities.unshift({
-			description: null,
-			icon: null,
-			name: null,
-			subreddit: {
-				detectedLanguage: 'en',
-				id: 't2_dakg11x',
-				isSubscribed: false,
-				path: '/r/PlacePride/',
-				prefixedName: 'r/PlacePride',
-				publicDescriptionText: 'Welcome to r/PlacePride! Our discord server can be found at https://discord.gg/MUMhGkK4un. We\'re an r/place faction dedicated to making pride related artwork on the canvas!',
-				styles: {
-					icon: 'https://styles.redditmedia.com/t5_63awgf/styles/communityIcon_auvdy43krvq81.png',
-					legacyIcon: null,
-					legacyPrimaryColor: null,
-					primaryColor: '#F7A8B8',
+	if (req.body?.variables?.eventKey === 'place_XZ') {
+		return { data: JSON.stringify({
+			data: {
+				navBarEventCommunityPicker: {
+					title: 'r/place communities',
+					subtitle: 'Check out some of the communities participating this year',
+					icon: {
+						url: 'https://i.redd.it/qwqv2vzjbp1b1.png',
+					},
+					communities: [
+						{
+							description: null,
+							icon: null,
+							name: null,
+							subreddit: {
+								detectedLanguage: 'en',
+								id: 't5_8ifl0y',
+								isSubscribed: false,
+								path: '/r/Save3rdPartyApps/',
+								prefixedName: 'r/Save3rdPartyApps',
+								publicDescriptionText: 'Reddit\'s recent changes to API pricing threaten to destroy user access to a huge variety of quality-of-life features exclusive to apps like Apollo, Narwhal and Reddit is Fun. We\'re here to stop it.',
+								styles: {
+									icon: 'https://styles.redditmedia.com/t5_8ifl0y/styles/communityIcon_m5cieh5vqf4b1.jpg',
+									legacyIcon: null,
+									legacyPrimaryColor: null,
+									primaryColor: '#BD5D4F',
+								},
+							},
+						},
+						{
+							description: null,
+							icon: null,
+							name: null,
+							subreddit: {
+								detectedLanguage: 'en',
+								id: 't5_63awgf',
+								isSubscribed: false,
+								path: '/r/PlacePride/',
+								prefixedName: 'r/PlacePride',
+								publicDescriptionText: 'Welcome to r/PlacePride! Our discord server can be found at https://discord.gg/MUMhGkK4un. We\'re an r/place faction dedicated to making pride related artwork on the canvas!',
+								styles: {
+									icon: 'https://styles.redditmedia.com/t5_63awgf/styles/communityIcon_auvdy43krvq81.png',
+									legacyIcon: null,
+									legacyPrimaryColor: null,
+									primaryColor: '#F7A8B8',
+								},
+							},
+						},
+						{
+							description: null,
+							icon: null,
+							name: null,
+							subreddit: {
+								detectedLanguage: 'en',
+								id: 't5_3ju2f',
+								isSubscribed: false,
+								path: '/r/osuplace/',
+								prefixedName: 'r/osuplace',
+								publicDescriptionText: 'The official subreddit of the osu! faction on /r/place!\n\n***\n\n**WE DO NOT USE AUTOMATION**\n\n**WE HAVE NEVER USED IT**\n\n**YOU WILL BE BANNED IF YOU USE BOTS**\n\nWe take pride in legitimately placing every single pixel!\n\nJoin us on [**Discord**](https://discord.gg/NdMsJfCjkN)!\n\n***',
+								styles: {
+									icon: null,
+									legacyIcon: null,
+									legacyPrimaryColor: null,
+									primaryColor: null,
+								},
+							},
+						},
+						{
+							description: null,
+							icon: null,
+							name: null,
+							subreddit: {
+								detectedLanguage: 'en',
+								id: 't5_3deum',
+								isSubscribed: false,
+								path: '/r/AprilKnights/',
+								prefixedName: 'r/AprilKnights',
+								publicDescriptionText: '"Ever vigilant, armed with patience and necromancy, the knights stand by."',
+								styles: {
+									icon: 'https://styles.redditmedia.com/t5_3deum/styles/communityIcon_ltxfnwrbeta81.png',
+									legacyIcon: null,
+									legacyPrimaryColor: null,
+									primaryColor: null,
+								},
+							},
+						},
+						{
+							description: null,
+							icon: null,
+							name: null,
+							subreddit: {
+								detectedLanguage: 'en',
+								id: 't5_3jsax',
+								isSubscribed: false,
+								path: '/r/TheBlueCorner/',
+								prefixedName: 'r/TheBlueCorner',
+								publicDescriptionText: 'The glorious Blue Corner shall rise again. Join us as we await the return of Reddit\'s April Fool\'s Event this year.',
+								styles: {
+									icon: 'https://styles.redditmedia.com/t5_3jsax/styles/communityIcon_bpyvma146bra1.png',
+									legacyIcon: null,
+									legacyPrimaryColor: null,
+									primaryColor: null,
+								},
+							},
+						},
+					],
 				},
 			},
-		});
+		}) };
 	}
 	return { data: JSON.stringify(data) };
 });
@@ -89,10 +178,14 @@ realtimeGQL2.injectBuffer((req, buffer) => {
 		return { data: JSON.stringify({ data: { act: { data: [{ __typename: 'GetUserCooldownResponseMessageData', data: { nextAvailablePixelTimestamp: new Date().getTime() + 300 * 1000 } }] } }, errors: [] }) };
 	}
 
-	// Return with r/PlacePride's coordinates
+	// Return with subreddit coordinates
 	if (req.body.query.includes('r/replace:get_subreddits_coordinates') || req.body.query.includes('r\\/replace:get_subreddits_coordinates')) {
 		const subredditIDs = req.body.variables.subredditIDs ? req.body.variables.subredditIDs : req.body.variables.subredditIds;
-		if (subredditIDs.includes('t5_63awgf')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_63awgf', coordinates: { x: 702, y: 614, z: 1 } }] } }] } }, errors: [] }) };
+		if (subredditIDs.includes('t5_8ifl0y')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_8ifl0y', coordinates: { x: 500, y: 500, z: 1 } }] } }] } }, errors: [] }) };
+		if (subredditIDs.includes('t5_63awgf')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_63awgf', coordinates: { x: 727, y: 614, z: 1 } }] } }] } }, errors: [] }) };
+		if (subredditIDs.includes('t5_3ju2f')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_3ju2f', coordinates: { x: 727, y: 727, z: 1 } }] } }] } }, errors: [] }) };
+		if (subredditIDs.includes('t5_3deum')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_3deum', coordinates: { x: 690, y: 733, z: 1 } }] } }] } }, errors: [] }) };
+		if (subredditIDs.includes('t5_3jsax')) return { data: JSON.stringify({ data: { act: { data: [{ data: { subredditsCoordinates: [{ subredditID: 't5_3jsax', coordinates: { x: 999, y: 999, z: 1 } }] } }] } }, errors: [] }) };
 	}
 	return { data: buffer.toString() };
 });
